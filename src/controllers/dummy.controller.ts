47292a0cbb5821d760b25baa07651cc82647ba44
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-07-29 15:06:45 +0700

    chore: update gitignore

--- src/controllers/dummy.controller.ts
+++ src/controllers/dummy.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export default {
   dummy(req: Request, res: Response) {
     res.status(200).json({
-      message: "Sucess hit endpoint dummy",
+      message: "Success hit endpoint dummy",
       data: "OK GAS",
     });
   },
